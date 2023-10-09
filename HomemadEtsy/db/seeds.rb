9b3b83ef1a89719c90e381ddcea57c317fef4660
David <2018dpollack@gmail.com>
2023-10-09 15:00:00 -0700

    Created seeds

--- HomemadEtsy/db/seeds.rb
+++ HomemadEtsy/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do
+    puts "Destroying users..."
+    User.destroy_all
+
+    puts "Resetting primary keys for users..."
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+
+    puts "Creating users..."
+    User.create!(
+        username: "HomemadEtsyDemoUser",
+        email: 'homemadEtsy@user.io',
+        password: 'password'
+    )
+
+    10.times do 
+        User.create!({
+            username: Faker::Internet.unique.username(specifier: 3..20),
+            email: Faker::Internet.unique.email,
+            password: 'password'
+        })
+    end
+
+    puts "Done!"
+end
