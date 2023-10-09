d9817b02d46337415f9a6b72a9b923c838e65b9b
David <2018dpollack@gmail.com>
2023-10-09 14:51:27 -0700

    User model done

--- HomemadEtsy/app/models/user.rb
+++ HomemadEtsy/app/models/user.rb
@@ -1,3 +1,57 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
-  has_secure_password
+
+  validates :username, presence: true, uniqueness: true, length: { in: 3..30}, format: {without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email"}
+  validates :email, presence: true, uniqueness: true, length: { in: 3..255 }, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: {in: 6..255}, allow_nil: true
+
+  has_secure_password # this handles password getter, setter, and is_password? for us
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(credential, password)
+    if credential.match?(URI::MailTo::EMAIL_REGEXP)
+      user = User.find_by(email: credential)
+    else
+      user = User.find_by(username: credential)
+    end
+
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
+
+
+  private
+
+  def generate_unique_session_token
+    while true
+      token = SecureRandom.urlsafe_base64
+      return token unless User.exists?(session_token: token)
+    end
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
 end
