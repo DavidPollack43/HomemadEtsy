65f2df68549be3014d1c28cac2e7b16e551a05e4
David <2018dpollack@gmail.com>
2023-10-13 09:47:59 -0700

    products controller done

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category, :user).all
     render :index
   end
 
@@ -10,23 +10,39 @@ class Api::ProductsController < ApplicationController
     if @product.save
       render :show
     else
-      render json: @post.errors.full_messages, status: 422
+      render json: @product.errors.full_messages, status: 422
     end
   end
 
   def show
     @product = Product.includes(:category, :user).find_by(id: params[:id])
-    render :show
+    if @product
+      render :show
+    else
+      render json: ["Product is not found"], status: 404
+    end
   end
 
   def update
+    @product = Product.find_by(id: params[:id])
+    if @product && @product.update(product_params) 
+      render :show
+    else
+      render json: @product.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @product = Product.find_by(id: params[:id])
+    if @product && @product.destroy
+      render :index
+    else
+      render json: ["Product could not be deleted"], status: 422
+    end
   end
 
   private
   def product_params
-      params.require(:product).permit(:title, :price, :description, :stock_quantity)
+      params.require(:product).permit(:title, :price, :description, :stock_quantity, :category_id)
   end
 end
