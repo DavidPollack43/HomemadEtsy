88488c5a729e7d6e4a75b00d5b31bd9bfe48d781
David <2018dpollack@gmail.com>
2023-10-13 16:56:29 -0700

    Finished thunk actions and reducers

--- frontend/src/store/products.js
+++ frontend/src/store/products.js
@@ -20,3 +20,41 @@ export const receiveProduct = (payload) => {
 export const getProducts = (state) => state.products ? Object.values(state.products) : [];
 
 export const getProduct = (productId) => (state) => state.products ? state.products[productId] : null;
+
+export const fetchProducts = () => async (dispatch) => {
+    const res = await fetch("/api/products");
+    if(res.ok){
+        const data = await res.json();
+        dispatch(receiveProducts(data));
+    }else{
+        const errorMessage = await res.json();
+        console.error("Failed to fetch products", errorMessage.message || "Unknown error")
+    }
+}
+
+export const fetchProduct = (productId) => async(dispatch) => {
+    const res = await fetch(`/api/products/${productId}`);
+    if(res.ok){
+        const data = await res.json();
+        dispatch(receiveProduct(data));
+    }else{
+        const errorMessage = await res.json();
+        console.error("Failed to fetch product", errorMessage.message || "Unknown error");
+    }
+}
+
+export const productReducer = (state = {}, action) => {
+    const nextState = Object.assign({}, state);
+
+    switch (action.type) {
+        case RECEIVE_PRODUCTS:
+            return {...state, ...action.products}
+        case RECEIVE_PRODUCT:
+            nextState[action.product.id] = action.product;
+            return nextState;
+        default:
+            return state;
+    }
+}
+
+export default productReducer;
