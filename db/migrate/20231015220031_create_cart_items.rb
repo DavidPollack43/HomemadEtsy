3816b49ffefacd453edaa82b2073d9bddbaa488b
David <2018dpollack@gmail.com>
2023-10-15 15:05:57 -0700

    Created cart_items table

---
+++ db/migrate/20231015220031_create_cart_items.rb
@@ -0,0 +1,11 @@
+class CreateCartItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cart_items do |t|
+      t.references :user, null: false, foreign_key: {to_table: :users}
+      t.references :product, null: false, foreign_key: {to_table: :products}
+      t.integer :quantity, null: false
+      t.timestamps
+    end
+    add_index :cart_items, [:user_id, :product_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_13_031549) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_220031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+    t.index ["user_id", "product_id"], name: "index_cart_items_on_user_id_and_product_id", unique: true
+    t.index ["user_id"], name: "index_cart_items_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +56,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_13_031549) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "cart_items", "products"
+  add_foreign_key "cart_items", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
