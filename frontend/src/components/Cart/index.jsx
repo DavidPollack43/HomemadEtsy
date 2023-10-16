459e3691ab8e9ca7df7e4749f69475ca9d5e8137
David <2018dpollack@gmail.com>
2023-10-16 16:28:38 -0700

    Quantity updated

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { getCartItems } from "../../store/cart";
+import { getCartItems, updateToCart } from "../../store/cart";
+import { useState } from 'react';
+import { useEffect } from "react";
 
 export const Cart = (props) => {
+    const dispatch = useDispatch();
+    const [quantityForCart, setQuantityForCart] = useState({}) //Need to make item.quantity default, but item is defined in my map
     const cart = useSelector(getCartItems);
     console.log(cart)
+
+    useEffect(() =>{
+        const initialQuantities = {};
+        cart.forEach(item => {
+            initialQuantities[item.id] = item.quantity;
+        });
+        setQuantityForCart(initialQuantities)
+    },[cart])
+
+
+    const setQuantity = (e, itemId) => {
+        e.preventDefault();
+        const newQuantity = parseInt(e.target.value);
+
+        setQuantityForCart({
+            ...quantityForCart,
+            [itemId]: parseInt(e.target.value)
+        });
+
+        dispatch(updateToCart(itemId, newQuantity));
+    }
+
     return(
         <>
             {cart.map(item => (
@@ -14,6 +40,11 @@ export const Cart = (props) => {
                     <p>{item.product.description}</p>
                     <p>{item.product.price}</p>
                     <p>{item.quantity}</p>
+                    <select value={quantityForCart[item.id] || ""} onChange={(e) => setQuantity(e, item.id)}>
+                        {[...Array(item.product.stockQuantity)].map((_, idx) => (
+                            <option key={idx} value={idx+1}>{idx + 1}</option> // Added return here.
+                        ))}
+                    </select>
                 </div>
             ))}
         </>
