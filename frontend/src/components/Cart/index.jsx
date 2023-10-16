9f79ff13fba8be8e9476b7b309ed21bf2f31b7df
David <2018dpollack@gmail.com>
2023-10-16 15:24:19 -0700

    basic cart page

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { getCartItems } from "../../store/cart";
 
 export const Cart = (props) => {
+    const cart = useSelector(getCartItems);
+    console.log(cart)
     return(
         <>
-            <h1>Hello from Cart</h1>
+            {cart.map(item => (
+                <div key={item.id} className="cartItem">
+                    <h2>{item.product.title}</h2>
+                    <p>{item.product.username}</p>
+                    <p>{item.product.description}</p>
+                    <p>{item.product.price}</p>
+                    <p>{item.quantity}</p>
+                </div>
+            ))}
         </>
     )
 }
