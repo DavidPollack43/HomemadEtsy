43f68107e48108200807110b463b76602b0f282a
David <2018dpollack@gmail.com>
2023-10-18 23:17:53 -0700

    Started cart

--- frontend/src/components/Cart/index.jsx
+++ frontend/src/components/Cart/index.jsx
@@ -3,12 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { deleteCartItem, fetchCart, getCartItems, updateToCart } from "../../store/cart";
 import { useState } from 'react';
 import { useEffect } from "react";
+import "./index.css"
 
 export const Cart = (props) => {
     const dispatch = useDispatch();
     const [quantityForCart, setQuantityForCart] = useState({}) //Need to make item.quantity default, but item is defined in my map
     const cart = useSelector(getCartItems);
-    console.log(cart)
 
     useEffect(() =>{
         const initialQuantities = {};
@@ -44,23 +44,29 @@ export const Cart = (props) => {
     return (cart.length > 0) ? 
     (
         <>
-            {cart.map(item => (
-                <div key={item.id} className="cartItem">
-                    <h2>{item.product.title}</h2>
-                    <p>{item.product.username}</p>
-                    <p>{item.product.description}</p>
-                    <p>{item.product.price}</p>
-                    <p>{item.quantity}</p>
-                    <p>Total Price of Item: {item.product.price * item.quantity}</p>
-                    <select value={quantityForCart[item.id] || ""} onChange={(e) => setQuantity(e, item.id)}>
-                        {[...Array(item.product.stockQuantity)].map((_, idx) => (
-                            <option key={idx} value={idx+1}>{idx + 1}</option> // Added return here.
-                        ))}
-                    </select>
-                    <br/>
-                    <button className="DeleteButton" onClick={handleClick(item.id)}>Remove from Cart</button>
-                </div>
-            ))}
+        <h1>{cart.length} items in your cart</h1>
+            <div className="CartItemsDiv">           
+                {cart.map(item => (
+                    <div key={item.id} className="cartItem">
+                        <h2>{item.product.title}</h2>
+                        <div className="productPictureDivCart">
+                            <img src={item.product.photoUrl} alt="productPicture" className="productImageCart"/>
+                        </div>
+                        <p>{item.product.username}</p>
+                        <p>{item.product.description}</p>
+                        <p>{item.product.price}</p>
+                        <p>{item.quantity}</p>
+                        <p>Total Price of Item: {item.product.price * item.quantity}</p>
+                        <select value={quantityForCart[item.id] || ""} onChange={(e) => setQuantity(e, item.id)}>
+                            {[...Array(item.product.stockQuantity)].map((_, idx) => (
+                                <option key={idx} value={idx+1}>{idx + 1}</option> // Added return here.
+                            ))}
+                        </select>
+                        <br/>
+                        <button className="DeleteButton" onClick={handleClick(item.id)}>Remove from Cart</button>
+                    </div>
+                ))}
+            </div>
             <br/>
             <h2>Total Order Price = {totalPrice}</h2>
         </>
