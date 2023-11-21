cc259ca15b7c17be8c18ddfcf365bcb839e62a51
David <2018dpollack@gmail.com>
2023-11-20 18:59:00 -0600

    Added thunk actions

--- frontend/src/store/review.js
+++ frontend/src/store/review.js
@@ -28,3 +28,74 @@ export const removeReview = (reviewId) => {
 export const getReviews = (state) => state.reviews ? Object.values(state.reviews) : [];
 
 export const getReview = (reviewId) => (state) => state.reviews ? state.reviews[reviewId] : null;
+
+export const fetchReviews = (productId) => async(dispatch) => {
+    const res = await fetch(`api/products/${productId}/reviews`);
+    if(res.ok){
+        const data = await res.json();
+        dispatch(receiveReviews(data));
+    } else {
+        const errorMessage = await res.json();
+        console.error("Failed to fetch reviews", errorMessage.message || "Unkown error");
+    }
+}
+
+export const fetchReview = (productId, reviewId) => async(dispatch) => {
+    const res = await fetch(`/api/products/${productId}/reviews/${reviewId}`);
+    if(res.ok){
+        const data = await res.json();
+        dispatch(receiveReview(data));
+    } else{
+        const errorMessage = await res.json();
+        console.error("Failed to fetch review", errorMessage.message || "Unkown error");
+    }
+}
+
+export const createReview = (productId, review) => async(dispatch) => {
+    const res = await csrfFetch(`/api/products/${productId}/reviews`, {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(review)
+    })
+    if(res.ok){
+        const data = await res.json();
+        dispatch(receiveReview(data));
+    } else {
+        const error = await res.json();
+        console.error("Error creating review: ", error.message);
+    }
+}
+
+export const updateReview = (productId, reviewId, review) => async(dispatch) => {
+    const res = await csrfFetch(`/api/products/${productId}/reviews/${reviewId}`, {
+        method: "PATCH", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(review)
+    })
+    if(res.ok){
+        const data = await res.json();
+        dispatch(receiveReview(data))
+    } else {
+        const error = await res.json();
+        console.error("Error updating review: ", error.message);
+    }
+}
+
+export const deleteReview = (productId, reviewId) => async(dispatch) => {
+    const res = await csrfFetch(`/api/products/${productId}/reviews/${reviewId}`, {
+        method: "DELETE", 
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    if(res.ok){
+        dispatch(removeReview(reviewId));
+    } else{
+        const error = await res.json();
+        console.error("Error deleting review: ", error.message);
+    }
+}
